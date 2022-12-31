f162c2ed3728783210c737e5fcae59b409ed1295
Tomoyuki UBE <ube@coder.okinawa.jp>
2022-12-31 22:33:38 +0900

    add supabase

--- save_supabase.py
+++ save_supabase.py
@@ -1,8 +1,27 @@
 import os
+import json
+import httpx
 from supabase import create_client, Client
+
+class supabaseWriter:
+    def write(self, supabase:Client):
+        with open("./dist/rank/all.json", "r", encoding="utf_8_sig") as f:
+            data = json.load(f)
+        if 'items' in data:
+            for item in data['items']:
+                try:
+                    supabase.table("card_summary").upsert(
+                        {
+                            "master_id": item['master_id'],
+                            "summary": item
+                        }
+                    ).execute()
+                except httpx.ReadTimeout:
+                    print("httpx.ReadTimeout"+item['master_id'])
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_ANON_KEY")
 supabase: Client = create_client(url, key)
-data = supabase.table("card_summary").insert({"master_id":"xxx","summary":{"data1":"abc","data2":123,"array":[1,2,3]}}).execute()
-assert len(data.data) > 0
+
+writer = supabaseWriter()
+writer.write(supabase)
