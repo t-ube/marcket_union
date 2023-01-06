892ad94f76af2b21bc0c18992689e2bd0daf69e4
Tomoyuki UBE <ube@coder.okinawa.jp>
2023-01-06 12:48:02 +0900

    add

--- save_supabase.py
+++ save_supabase.py
@@ -6,81 +6,99 @@ from supabase import create_client, Client
 
 class supabaseWriter:
     def writeSummary(self, supabase:Client):
+        timestamp = datetime.datetime.utcnow()
         with open("./dist/rank/all.json", "r", encoding="utf_8_sig") as f:
             data = json.load(f)
         if 'items' in data:
-            for item in data['items']:
+            # 100 個ずつに分解する
+            for i in range(0, len(data['items']), 100):
+                batch = data['items'][i: i+100]
+                print('Write summary no.:'+str(i))
+                batch_item = []
+                for item in batch:
+                    batch_item.append({
+                        "master_id": item['master_id'],
+                        "summary": item,
+                        "updated_at": timestamp.strftime('%Y-%m-%d %H:%M:%S+00'),
+                    })
                 try:
-                    supabase.table("card_summary").upsert(
-                        {
-                            "master_id": item['master_id'],
-                            "summary": item
-                        }
-                    ).execute()
+                    supabase.table("card_summary").upsert(batch_item).execute()
                 except httpx.ReadTimeout:
-                    print("httpx.ReadTimeout"+item['master_id'])
+                    print("httpx.ReadTimeout")
 
     def writeRecipe(self, supabase:Client):
         timestamp = datetime.datetime.utcnow()
         with open("./dist/recipe/deck_recipe_all.json", "r", encoding="utf_8_sig") as f:
             data = json.load(f)
         if 'items' in data:
-            for item in data['items']:
+            # 20 個ずつに分解する
+            for i in range(0, len(data['items']), 20):
+                batch = data['items'][i: i+20]
+                print('Write recipe no.:'+str(i))
+                batch_item = []
+                for item in batch:
+                    batch_item.append({
+                        "deck_id": item['deck_id'],
+                        "event_id": item['event_id'],
+                        "deck_type": item['deck_type'],
+                        "datetime": item['datetime']+"+09",
+                        "rank": item['rank'],
+                        "event_name": item['event_name'],
+                        "sponsorship": item['sponsorship'],
+                        "player_name": item['player_name'],
+                        "items": item['items'],
+                        "updated_at": timestamp.strftime('%Y-%m-%d %H:%M:%S+00'),
+                        "count": item['count'],
+                        "card_type": item['card_type'],
+                        "regulation": item['regulation'],
+                        "price": item['price']
+                    })
                 try:
-                    supabase.table("deck_recipe").upsert(
-                        {
-                            "deck_id": item['deck_id'],
-                            "event_id": item['event_id'],
-                            "deck_type": item['deck_type'],
-                            "datetime": item['datetime']+"+09",
-                            "rank": item['rank'],
-                            "event_name": item['event_name'],
-                            "sponsorship": item['sponsorship'],
-                            "player_name": item['player_name'],
-                            "items": item['items'],
-                            "updated_at": timestamp.strftime('%Y-%m-%d %H:%M:%S+00'),
-                            "count": item['count'],
-                            "card_type": item['card_type'],
-                            "regulation": item['regulation'],
-                            "price": item['price']
-                        }
-                    ).execute()
+                    supabase.table("deck_recipe").upsert(batch_item).execute()
                 except httpx.ReadTimeout:
-                    print("httpx.ReadTimeout"+item['deck_id'])
+                    print("httpx.ReadTimeout")
     
     def writeChart(self, supabase:Client):
         timestamp = datetime.datetime.utcnow()
         with open("./dist/chart/all_line_charts.json", "r", encoding="utf_8_sig") as f:
             data = json.load(f)
         if 'items' in data:
-            for item in data['items']:
+            # 100 個ずつに分解する
+            for i in range(0, len(data['items']), 100):
+                batch = data['items'][i: i+100]
+                print('Write chart no.:'+str(i))
+                batch_item = []
+                for item in batch:
+                    batch_item.append({
+                        "master_id": item['master_id'],
+                        "chart_line": item['chart_line'],
+                        "updated_at": timestamp.strftime('%Y-%m-%d %H:%M:%S+00'),
+                    })
                 try:
-                    supabase.table("card_chart").upsert(
-                        {
-                            "master_id": item['master_id'],
-                            "chart_line": item['chart_line'],
-                            "updated_at": timestamp.strftime('%Y-%m-%d %H:%M:%S+00'),
-                        }
-                    ).execute()
+                    supabase.table("card_chart").upsert(batch_item).execute()
                 except httpx.ReadTimeout:
-                    print("httpx.ReadTimeout"+item['master_id'])
+                    print("httpx.ReadTimeout")
 
     def writeLog(self, supabase:Client):
         timestamp = datetime.datetime.utcnow()
         with open("./dist/log/market_price_log.json", "r", encoding="utf_8_sig") as f:
             data = json.load(f)
         if 'items' in data:
-            for item in data['items']:
+            # 100 個ずつに分解する
+            for i in range(0, len(data['items']), 100):
+                batch = data['items'][i: i+100]
+                print('Write log no.:'+str(i))
+                batch_item = []
+                for item in batch:
+                    batch_item.append({
+                        "master_id": item['master_id'],
+                        "log": item['log'],
+                        "updated_at": timestamp.strftime('%Y-%m-%d %H:%M:%S+00'),
+                    })
                 try:
-                    supabase.table("card_price_log").upsert(
-                        {
-                            "master_id": item['master_id'],
-                            "log": item['log'],
-                            "updated_at": timestamp.strftime('%Y-%m-%d %H:%M:%S+00'),
-                        }
-                    ).execute()
+                    supabase.table("card_price_log").upsert(batch_item).execute()
                 except httpx.ReadTimeout:
-                    print("httpx.ReadTimeout"+item['master_id'])
+                    print("httpx.ReadTimeout")
 
     def clearFiles(self):
         os.remove('./dist/rank/all.json')
@@ -91,6 +109,8 @@ class supabaseWriter:
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_ANON_KEY")
+
+
 supabase: Client = create_client(url, key)
 
 writer = supabaseWriter()
